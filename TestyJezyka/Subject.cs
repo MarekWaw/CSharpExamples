a95172100aaa28c0425c9ee5ffafb36582c79b9c
Marek Wawrzynczyk <Marek Wawrzynczyk>
2017-06-04 15:31:43 +0200

    Submit

---
+++ TestyJezyka/Subject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestyJezyka
+{
+    public class Subject
+    {
+        public event EventHandler procesorZdarzen;
+        public void DajZnacObserwatorowi()
+        {
+            if (procesorZdarzen != null)
+            {
+                procesorZdarzen(this, EventArgs.Empty); // Lub własny komunikat
+            }
+        }
+    }
+}
