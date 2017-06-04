1c372718e91b40b181b621b3f52184384a2d0f97
Marek Wawrzynczyk <Marek Wawrzynczyk>
2017-06-04 15:32:54 +0200

    Obserwer

---
+++ TestyJezyka/Observer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestyJezyka
+{
+    public class Observer
+    {
+        Subject obiekt;
+        public Observer(Subject obiekt)
+        {
+            this.obiekt = obiekt;
+        }
+        public void Subskrybuj()
+        {
+            obiekt.procesorZdarzen += ZrobCos;
+        }
+        public void Rezygnuj()
+        {
+            obiekt.procesorZdarzen -= ZrobCos;
+        }
+        private void ZrobCos(object sender, EventArgs e)
+        {
+            Console.WriteLine("Notyfikacja z obiektu");
+        }
+    }
+}
