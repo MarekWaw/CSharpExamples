c2cc02011b1d427ed07590397971b67fef5db4bf
Marek Wawrzynczyk <Marek Wawrzynczyk>
2017-06-04 15:24:11 +0200

    ../.vs/ 	../App1/ 	../Komunikacja/ 	../TestAppDomain/ 	../TestyJezyka.sln 	../TestyJezyka.suo 	Observer.cs 	Properties/ 	Subject.cs 	TestyJezyka.csproj 	bin/ 	obj/ 	../UruAppDomian/ 	../Zdarzenia/

---
+++ TestyJezyka/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestyJezyka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject obiekt = new Subject();
+            Observer obserwator1 = new Observer(obiekt);
+            Observer obserwator2 = new Observer(obiekt);
+
+            obserwator1.Subskrybuj();
+            obiekt.DajZnacObserwatorowi();
+
+            Console.ReadKey();
+
+        }
+    }
+}
